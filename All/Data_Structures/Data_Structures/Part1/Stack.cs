59f4d6eec864d7e0b982dd29ca180ac123c70b56
Ali Chavoshi <alichavoshii1372@gmail.com>
2021-01-26 10:20:52 +0330

    update stack.

--- All/Data_Structures/Data_Structures/Part1/Stack.cs
+++ All/Data_Structures/Data_Structures/Part1/Stack.cs
@@ -4,28 +4,34 @@ namespace Data_Structures.Part1
 {
     public class Stack
     {
-        private int[] _items = new int[5];
-        private int count = 0;
+        private readonly int[] _items = new int[5];
+        private int _count;
         public void Push(int item)
         {
-            if (count == _items.Length)
+            if (IsFull())
             {
                 throw new Exception("Stack Is Full");
             }
-            _items[count] = item;
-            count++;
+            _items[_count++] = item;
+        }
+        public bool IsEmpty()
+        {
+            return _count == 0;
+        }
+        public bool IsFull()
+        {
+            return _count == _items.Length;
         }
         public int Pop()
         {
-            if (count == 0)
+            if (IsEmpty())
                 throw new Exception("Stack Is Empty");
 
-            var item = _items[count - 1];
-            count--;
-            _items[count] = 0;
+            var item = _items[_count - 1];
+            _count--;
+            _items[_count] = 0;
             return item;
         }
-
         public void PrintIn()
         {
             foreach (var item in _items)
@@ -33,10 +39,9 @@ namespace Data_Structures.Part1
                 Console.WriteLine(item);
             }
         }
-
         public int Peek()
         {
-            return _items[count - 1];
+            return _items[_count - 1];
         }
     }
 
