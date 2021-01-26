fc5fd54f16c0b5d4f7673f869bac0c8781957056
Ali Chavoshi <alichavoshii1372@gmail.com>
2021-01-26 10:34:52 +0330

    Add ReverseQueueByStack

---
+++ All/Data_Structures/Data_Structures/Part1/ReverseQueueByStack.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Data_Structures.Part1
+{
+    public class ReverseQueueByStack
+    {
+        public static void Reverse(Queue<int> queues)
+        {
+            var stacks = new Stack<int>();
+            while (queues.Any())
+            {
+                stacks.Push(queues.Dequeue());
+            }
+
+            while (stacks.Any())
+            {
+                queues.Enqueue(stacks.Pop());
+            }
+        }
+    }
+}
