63d338fe5603bd9ec1ca8cef5093d7400f62f8d7
Ali Chavoshi <alichavoshii1372@gmail.com>
2021-01-26 10:20:39 +0330

    update linked list

--- All/Data_Structures/Data_Structures/Part1/LinkedList.cs
+++ All/Data_Structures/Data_Structures/Part1/LinkedList.cs
@@ -5,29 +5,29 @@ namespace Data_Structures.Part1
 {
     public class LinkedList
     {
-        private Node first;
-        private Node last;
-        private int size;
+        private Node _first;
+        private Node _last;
+        private int _size;
         private class Node
         {
-            private int value;
-            internal Node next;
-            public Node(int value) => this.value = value;
-            public int GetValue() => value;
+            private readonly int _value;
+            internal Node Next;
+            public Node(int value) => this._value = value;
+            public int GetValue() => _value;
         }
         //isEmpty
-        private bool IsEmpty() => first == null;
+        private bool IsEmpty() => _first == null;
         //GetPrevious
         private Node GetPrevious(Node node)
         {
-            var current = first;
+            var current = _first;
             while (current != null)
             {
-                if (current.next == node)
+                if (current.Next == node)
                 {
                     return current;
                 }
-                current = current.next;
+                current = current.Next;
             }
             return null;
         }
@@ -37,14 +37,14 @@ namespace Data_Structures.Part1
             var node = new Node(value);
             if (IsEmpty())
             {
-                first = last = node;
+                _first = _last = node;
             }
             else
             {
-                node.next = first;
-                first = node;
+                node.Next = _first;
+                _first = node;
             }
-            size++;
+            _size++;
         }
         //Add Last
         public void AddLast(int value)
@@ -52,14 +52,14 @@ namespace Data_Structures.Part1
             var node = new Node(value);
             if (IsEmpty())
             {
-                first = last = node;
+                _first = _last = node;
             }
             else
             {
-                last.next = node;
-                last = node;
+                _last.Next = node;
+                _last = node;
             }
-            size++;
+            _size++;
         }
         //Delete First
         public void RemoveFirst()
@@ -68,34 +68,32 @@ namespace Data_Structures.Part1
             {
                 throw new NoNullAllowedException();
             }
-            if (first == last)
+            if (_first == _last)
             {
-                first = last = null;
-                size--;
+                _first = _last = null;
+                _size--;
                 return;
             }
-            var second = first.next;
-            first.next = null;
-            first = second;
-            size--;
+            var second = _first.Next;
+            _first.Next = null;
+            _first = second;
+            _size--;
         }
         //Delete Last
         public void RemoveLast()
         {
             if (IsEmpty())
-            {
                 throw new Exception();
-            }
-            if (first == last)
+            if (_first == _last)
             {
-                first = last = null;
-                size--;
+                _first = _last = null;
+                _size--;
                 return;
             }
-            var previous = GetPrevious(last);
-            last = previous;
-            last.next = null;
-            size--;
+            var previous = GetPrevious(_last);
+            _last = previous;
+            _last.Next = null;
+            _size--;
         }
         //Contains
         public bool Contains(int value)
@@ -106,7 +104,7 @@ namespace Data_Structures.Part1
         public int IndexOf(int value)
         {
             var index = 0;
-            var current = first;
+            var current = _first;
             while (current != null)
             {
                 if (current.GetValue() == value)
@@ -114,7 +112,7 @@ namespace Data_Structures.Part1
                     return index;
                 }
                 index++;
-                current = current.next;
+                current = current.Next;
             }
             return -1;
         }
@@ -138,19 +136,19 @@ namespace Data_Structures.Part1
         //Size O(1)
         public int Size()
         {
-            return size;
+            return _size;
         }
         //To Array
         public int[] ToArray()
         {
-            var array = new int[size];
+            var array = new int[_size];
             var index = 0;
-            var current = first;
+            var current = _first;
             while (current != null)
             {
                 array[index] = current.GetValue();
                 index++;
-                current = current.next;
+                current = current.Next;
             }
             return array;
         }
@@ -160,43 +158,43 @@ namespace Data_Structures.Part1
             if (IsEmpty())
                 return;
 
-            var privouse = first;
-            var current = first.next;
+            var privouse = _first;
+            var current = _first.Next;
             while (current != null)
             {
-                var next = current.next;
+                var next = current.Next;
                 //[10 <- 20  30]
                 //p     c    n
-                current.next = privouse;
+                current.Next = privouse;
                 //[10 <- 20  30]
                 //       p    c    n
                 privouse = current;
                 current = next;
             }
 
-            last = first;
-            last.next = null;
-            first = privouse;
+            _last = _first;
+            _last.Next = null;
+            _first = privouse;
         }
         //getKthFromEnd
         public int GetKthFromEnd(int k)
         {
             if (IsEmpty())
                 throw new Exception("kthe empty list");
-            if (k > size)
+            if (k > _size)
                 throw new Exception("The k biger than the size");
-            var a = first;
-            var b = first;
+            var a = _first;
+            var b = _first;
             for (var i = 0; i < k - 1; i++)
             {
-                b = b.next;
+                b = b.Next;
                 if (b == null)
                     throw new Exception("The k biger than the size");
             }
-            while (b != last)
+            while (b != _last)
             {
-                a = a.next;
-                b = b.next;
+                a = a.Next;
+                b = b.Next;
             }
             return a.GetValue();
         }
