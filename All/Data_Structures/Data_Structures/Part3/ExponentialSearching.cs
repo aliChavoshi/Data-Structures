0b6aca985fee94fb4a0b5c2c4e3625877bc17892
Ali <alichavoshii1372@gmail.com>
2021-02-03 18:46:01 +0330

    Add Exponential Search

---
+++ All/Data_Structures/Data_Structures/Part3/ExponentialSearching.cs
@@ -0,0 +1,25 @@
+﻿namespace Data_Structures.Part3
+{
+    public class ExponentialSearching
+    {
+        public int Searching(int[] array, int target)
+        {
+            var bound = 1;
+            while (bound < array.Length && array[bound] < target)
+            {
+                bound *= 2;
+            }
+
+            if (bound > array.Length)
+            {
+                bound = array.Length-1;
+            }
+            for (var i = bound / 2; i < bound; i++)
+            {
+                if (array[i] == target)
+                    return i;
+            }
+            return -1;
+        }
+    }
+}
